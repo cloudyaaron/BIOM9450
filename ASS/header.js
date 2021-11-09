b78830048d235a6572be7157d0e49039ad74026e
cloudyaaron <cloudyaaron@users.noreply.github.com>
2021-11-09 16:32:22 +1100

    login page

---
+++ ASS/header.js
@@ -0,0 +1,17 @@
+const header = '\
+<div class="title">\
+    <img src="ServiceUNSW.png" alt="Service UNSW" width="40" height="40" align="left">\
+    <h1 align="left"> Medication and Diet Regime Management System </h1>\
+</div>\
+\
+<div class="navbar">\
+    <div class=" tooltip" > \
+        <a href="index.html" class="plain">Home</a>\
+    </div >\
+    <div class=" tooltip">\
+        <a href="login.html" class="plain">Employee Login</a>\
+        <span class=" tooltiptext">Extra features once login</span>\
+    </div>\
+ </div>';
+
+document.getElementById("headnav").innerHTML = header;
