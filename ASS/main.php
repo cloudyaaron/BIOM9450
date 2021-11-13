6a42249819210237c2d5cbce6a497b7e5db182fb
cloudyaaron <cloudyaaron@users.noreply.github.com>
2021-11-13 17:34:55 +1100

    Sign in done
    connected to database

--- ASS/main.php
+++ ASS/main.php
@@ -33,34 +33,71 @@
 
             $valid = false;
 
+
             // if the request is noraml log in to the page
             if (!empty($_POST['username']) && !empty($_POST['password'])) {
                 $username = $_POST['username'];
                 $password = $_POST['password'];
                 $valid = true;
 
+
                 // validate user detail with database
                 $conn = odbc_connect("ass",'','',SQL_CUR_USE_ODBC);
-                
+
                 // report a error if connection failed
                 if(!$conn){
                     echo "<div class='wr'>Internal Unkown Error, Plz contact us about this issue</div>";
                 }
                 
-                $sql_query = "SELECT * FROM Practitioner";
+                // Check if user exist and if password is correct
+                $sql_query = "SELECT * FROM Practitioner WHERE `UserName` = '$username'";
                 $result = odbc_exec($conn,$sql_query) or die(odbc_errormsg());
-                while(odbc_fetch_row($result)){
-                    $FN = odbc_result($result,"FirstName");
-                    $LN = odbc_result($result,"LastName");
-                    echo "<div class='firstcol'>$FN $LN</div>";
+                $result_array = odbc_fetch_array($result);
 
+                // User not exist, this should not give user info for safty consideration
+                if (!$result_array) {
+                    $valid = false;
+                    
+                // user Exist
+                }else{
+
+                    // Generate a token if everthing is valid
+                    if ($result_array['UserName'] == $username && $result_array['Password'] == $password) {
+                        // generates a token
+                        $t = openssl_random_pseudo_bytes(16);
+                        $t = bin2hex($t);
+                        setcookie("token",$t);
+
+                        // Registrate token in database
+                        $practionerID = $result_array['PractitionerID'];
+                        $d = strtotime("now .GMT-9");
+                        $now = date("Y-m-d h:i:sa",$d);
+
+                        // check if current database has token
+                        // replace with the new token
+                        $sql_query = "SELECT * FROM [LoginStatus] WHERE `PractitionerID` = $practionerID";
+                        $result = odbc_exec($conn,$sql_query) or die(odbc_errormsg());
+                        $result_array = odbc_fetch_array($result);
+
+                        // if no token
+                        if (!$result_array) {
+                            $sql_insert = "INSERT INTO [LoginStatus] (`PractitionerID`,`Token`,`LastLoginTime`) VALUES('$practionerID','$t','$now')";
+                            $result = odbc_exec($conn,$sql_insert) or die(odbc_errormsg());
+
+                        // if token exist then update
+                        }else{
+                            $sql_delete = "DELETE FROM [LoginStatus] WHERE `PractitionerID` = $practionerID";
+                            $result = odbc_exec($conn,$sql_delete) or die(odbc_errormsg());
+
+                            $sql_insert = "INSERT INTO [LoginStatus] (`PractitionerID`,`Token`,`LastLoginTime`) VALUES('$practionerID','$t','$now')";
+                            $result = odbc_exec($conn,$sql_insert) or die(odbc_errormsg());
+                        }
+ 
+                    }
                 }
+
                 odbc_close($conn);
 
-                // generates a token
-                $t = openssl_random_pseudo_bytes(16);
-                $t = bin2hex($t);
-                setcookie("token",$t);
 
             // else request can be either direct visit or contain a token
             } else{
